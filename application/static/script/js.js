469252a11f5aa65d58a6a8d5b57cd8a16d3f7446
chinazom <angelaaduradu@gmail.com>
2025-01-19 10:35:34 +0100

    i made a few adustments to the view_more.html

--- application/static/script/js.js
+++ application/static/script/js.js
@@ -5,3 +5,10 @@ for (let i = 0; i < view_btns.length; i++) {
     window.location.href = "/view_more";
   };
 }
+document.querySelectorAll('.pages a').forEach(link => {
+  link.addEventListener('click', event => {
+      event.preventDefault();
+      const targetId = link.getAttribute('href').substring(1);
+      document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+  });
+});
