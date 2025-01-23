7656ecf9255a4fa14fad7331445f28a7400ea70f
Aduradu Chinazom <angelaaduradu@gmail.com>
2025-01-23 19:56:03 +0100

    i did something

--- application/static/script/view_more.js
+++ application/static/script/view_more.js
@@ -2,17 +2,25 @@ document.addEventListener("DOMContentLoaded", () => {
   const office = JSON.parse(sessionStorage.getItem("selectedOffice"));
 
   if (office) {
-    document.querySelector(".head_text").textContent = office.name;
-    document.querySelector(".view_img").src = office.image;
-    document.querySelector(".name").textContent = office.head;
+      document.querySelector(".head_text").textContent = office.name;
+      document.querySelector(".view_img").src = office.image;
+      document.querySelector(".name").textContent = office.head;
   } else {
-    alert("Office data not found!");
-    window.location.href = "/";
+      alert("Office data not found!");
+      window.location.href = "/";
   }
 });
 
 let send = document.getElementById("send");
 
-  view_btns.onclick = function () {
-    window.location.href = "/success";
-  };
+send.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const messageBox = document.getElementById("message");
+  const message = messageBox.value.trim();
+  if (message) {
+      window.location.href = "/success";
+  } else {
+      alert("Please fill in the message box before sending!");
+  }
+});
